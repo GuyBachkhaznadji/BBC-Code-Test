99d2fe33c82ad95b121eff9d9594f87fcb8c6adc
Guy Bachkhaznadji <guysergebach@hotmail.com>
2017-09-16 22:12:36 +0100

    written unit tests definitions

--- models/specs/http_reporter_spec.rb
+++ models/specs/http_reporter_spec.rb
@@ -9,8 +9,67 @@ Minitest::Reporters.use!( Minitest::Reporters::SpecReporter.new )
 describe "Http Reporter" do
 
   before do
-  
+    @all_valid_http_string = "https://www.bbc.co.uk \n https://www.theguardian.com/uk"
   end
 
+  it " Should separate the URLs by line" do
+    skip
+  end
+
+  it " Should make a http GET request to the URL" do
+    skip
+  end
+
+  it " Should check that the URL is valid" do
+    skip
+  end
+
+  it " Should record the status code" do
+    skip
+  end
+
+  it " Should record the content length" do
+    skip
+  end
+
+  it " Should record the date-time of the response" do
+    skip
+  end
+
+  it " Should convert the request's details as JSON" do
+    skip
+  end
+
+  it " Should identify invalid URLs" do
+    skip
+  end
+
+  it " Should record error message for invalid URL" do
+    skip
+  end
+
+  it " Should output JSON with different details for invalid URLs" do
+    skip
+  end
+
+  it " Should not crash if web server is not responsive" do
+    skip
+  end
+
+  it " Should timeout request after 10 seconds" do
+    skip
+  end
+
+  it " Should count all results by status code" do
+    skip
+  end
+
+  it " Should output an array of status code counts" do
+    skip
+  end
+
+  it " Should output JSON array of status codes" do
+    skip
+  end
 
 end
