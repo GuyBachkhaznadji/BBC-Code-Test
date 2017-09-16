c50b8d8ff5b674ad50983b5ed87a1325c78e355b
Guy Bachkhaznadji <guysergebach@hotmail.com>
2017-09-16 23:09:35 +0100

    added some basic UI to do manual testing

--- app.rb
+++ app.rb
@@ -4,3 +4,7 @@ http_reporter = HttpReporter.new()
 
 puts("\nPlease enter the http requests you would like the details of:")
 http_string = gets.chomp
+response = http_reporter.http_request(http_string)
+summary = http_reporter.generate_summary(http_string, response)
+
+puts summary
